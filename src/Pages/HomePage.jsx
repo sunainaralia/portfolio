9bf70e73cb3bdbcd943fcb3a47eb71ba733ff9e4
sunainaralia <124876937+sunainaralia@users.noreply.github.com>
2024-09-30 10:43:13 +0530

    first

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import header_bg from '../assets/header_bg.png';
+import { NavBar } from '../Components/NavBar';
+export const HomePage = () => {
+  return (
+    <header className='bg_img min-h-[100vh]'>
+      <NavBar />
+    </header>
+  )
+}
